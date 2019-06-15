e58e03b3f41ea75cffe9bd51e08b924d8d1cb5ac
telepatheticman <telepatheticman13@gmail.com>
2019-06-15 12:42:30 -0700

    add some counting code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void count(std::string path, int countTo){
+    for(int x = 0; x <= countTo; x++){
+        //checkpoint
+        //increment
+        //wait
+        std::cout << x << std::endl << std::flush;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+    count("", 100);
+
     return 0;
 }
