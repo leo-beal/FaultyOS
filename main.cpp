65f29b256143624836f942025135341e277d78d5
telepatheticman <telepatheticman13@gmail.com>
2019-06-15 15:04:22 -0700

    try mutex fork
    Does not work

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <sys/unistd.h>
+#include <mutex>
+
+std::mutex oneCount;
 
 void count(std::string path, int countTo){
+    oneCount.lock();
     for(int x = 0; x <= countTo; x++){
         //checkpoint
         //increment
@@ -10,10 +15,12 @@ void count(std::string path, int countTo){
         std::cout << x << std::endl << std::flush;
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
+    oneCount.unlock();
 }
 
 int main(int argc, char* argv[]) {
 
+    fork();
     count("", 100);
 
     return 0;
